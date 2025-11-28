84c11d07a447b29ac5a8744114e5d0e46fc4e6c7
drewpager <drewpage@shredlights.com>
2025-11-28 07:18:53 -0800

    Replacing canonical URL

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -43,7 +43,7 @@ export const authOptions: NextAuthOptions = {
       ...session,
       user: {
         ...session.user,
-        stripeSubscriptionStatus: user.stripeSubscriptionStatus || "active",
+        stripeSubscriptionStatus: user.stripeSubscriptionStatus || "trialing",
         id: user.id,
       },
     }),
